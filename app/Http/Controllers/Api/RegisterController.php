2cb466ff059a531789aa87dde97bdbe5f0a062af
Ridho Anfa'al <ridhoanfaalkuliah@gmail.com>
2025-05-06 01:18:27 +0700

    Job sheet 10 - practicum 2

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -3,33 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use App\Models\UserModel;
 
 class RegisterController extends Controller
 {
     public function __invoke(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'username' => 'required',
-            'nama' => 'required',
-            'password' => 'required|min:5|confirmed',
-            'level_id' => 'required'
-        ]); 
+            "username" => "required",
+            "nama" => "required",
+            "password" => "required|min:5|confirmed",
+            "level_id" => "required",
+        ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
         $user = UserModel::create([
-            'username' => $request->username,
-            'nama' => $request->nama,
-            'password' => bcrypt($request->password),
-            'level_id' => $request->level_id,
+            "username" => $request->username,
+            "nama" => $request->nama,
+            "password" => bcrypt($request->password),
+            "level_id" => $request->level_id,
         ]);
 
-        if($user){
+        if ($user) {
             return response()->json([
                 'success' => true,
                 'user' => $user,
